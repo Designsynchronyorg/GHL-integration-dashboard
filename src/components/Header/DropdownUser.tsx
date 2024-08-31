05f11107643af70a0cdc1911df5fb9c3bffeb4c2
Abi <64946264+abreel@users.noreply.github.com>
2024-08-31 13:49:17 +0000

    Updated demo user dropdown info

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -21,9 +21,9 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
+            Mick Edwards
           </span>
-          <span className="block text-xs">UX Designer</span>
+          <span className="block text-xs">Web Designer</span>
         </span>
 
         <span className="h-12 w-12 rounded-full">
