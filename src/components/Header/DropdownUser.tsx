7394a3e0ec143fbaae4d0de384bd795a39eb83c1
Abi <64946264+abreel@users.noreply.github.com>
2024-08-31 10:41:46 +0000

    header updated

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -127,6 +127,9 @@ const DropdownUser = () => {
                 Account Settings
               </Link>
             </li>
+            <li>  
+            <DarkModeSwitcher />
+            </li>
           </ul>
           <button className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
             <svg
