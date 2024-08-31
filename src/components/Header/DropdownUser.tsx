97aca77cc653747ea172a4a95c91761723924962
Abi <64946264+abreel@users.noreply.github.com>
2024-08-31 11:03:23 +0000

    update dropdown icon

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -57,7 +57,7 @@ const DropdownUser = () => {
           className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark`}
         >
           <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-7.5 dark:border-strokedark">
-            <li>
+            {/* <li>
               <Link
                 href="/profile"
                 className="flex items-center gap-3.5 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
@@ -127,9 +127,12 @@ const DropdownUser = () => {
                 </svg>
                 Account Settings
               </Link>
-            </li>
+            </li> */}
             <li>  
-              <DarkModeSwitcher />
+              <div className="flex w-full justify-between">
+                <div>Theme</div>
+                <DarkModeSwitcher />
+              </div>
             </li>
           </ul>
           <button className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
