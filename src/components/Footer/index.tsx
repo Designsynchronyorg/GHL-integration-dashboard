7db9a8dcb613fde12bc1bc5be57e7654b81ed9de
Abi <64946264+abreel@users.noreply.github.com>
2024-08-31 12:03:11 +0000

    Setup footer and auth pages

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="w-full px-4 py-2">
+            <p className="text-muted text-sm text-center">Built By Mick & Abi</p>
+          </div>
+    )
+}
